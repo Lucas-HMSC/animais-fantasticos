8ca9efaabdec2a89ae9b14d1beb2b327d7271b19
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-23 17:51:25 -0300

    Create ex001.js

---
+++ dom-para-iniciantes/ex001.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const classeAtivo = document.querySelector('.ativo');
+console.log(classeAtivo);
+
+// Retorne a linguagem do navegador
+const idioma = window.navigator.language;
+console.log(idioma);
+
+// Retorne a largura da janela 
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
