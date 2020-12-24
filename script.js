5e5fe76aba78d01a0cdec2b32ed7b0c376c2cefd
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-24 09:37:51 -0300

    Create ex002

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*
+ * Exercícios realizados durante o módulo de DOM Para Iniciantes
+*/
+
+//=========================================================================
+/* Exercício 001  */
+
 // Retorne no console todas as imagens do site
 const imgs = document.getElementsByTagName('img');
 console.log(imgs);
@@ -17,3 +24,31 @@ console.log(firstH2);
 // Selecione o último p do site
 const lastP = document.querySelectorAll('p');
 console.log(lastP[lastP.length - 1]);
+
+//=========================================================================
+/* Exercício 002  */
+
+// Mostre no console cada parágrafo do site
+const paragrafos = document.querySelectorAll('p');
+paragrafos.forEach((p) => {
+  console.log(p);
+});
+
+// Mostre o texto dos parágrafos no console
+paragrafos.forEach((p) => {
+  console.log(p.innerText);
+});
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
