67f9abedcaec743bad3f536f040e5189e6715a6b
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-24 16:01:36 -0300

    Accordion List

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ function iniTabNav() {
   }
 }
 iniTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if (accordionList.length){
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    
+    
+    function activeAccordion(){
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
+initAccordion();
