748025ab103d2081f42cf96b649e16da26664490
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 17:20:02 -0300

    Modules 1

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,22 @@
+export default function initAnimationScroll() {
+  const scetions = document.querySelectorAll('[data-anime="scroll"]');
+
+  if (scetions.length){
+    const windowHalf = window.innerHeight * 0.6;
+  
+    function animaScroll(){
+      scetions.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowHalf) < 0
+        if (isSectionVisible){
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+    animaScroll();
+    
+    window.addEventListener('scroll', animaScroll);
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSmooth from './modules/scroll-smooth.js';
+import initAnimationScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import iniTabNav from './modules/tabnav.js';
+
+initScrollSmooth();
+initAnimationScroll();
+initAccordion();
+iniTabNav();
