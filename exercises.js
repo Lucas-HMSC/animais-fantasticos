ef96d359536849ca563690d3f7b92621d3102c8d
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 12:12:35 -0300

    Create ex013

--- exercises.js
+++ exercises.js
@@ -475,3 +475,61 @@ const cloneCarros = carros.slice();
 carros.pop();
 console.log(carros);
 console.log(cloneCarros);
+
+//=========================================================================
+/* Exercício 013  */
+
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
+const ex013 = [];
+cursos.forEach((item, index) => {
+  return ex013[index] = {
+    titulo: (item.querySelector('h1')).innerText,
+    descricao: (item.querySelector('p')).innerText,
+    aulas: (item.querySelector('.aulas')).innerText,
+    horas: (item.querySelector('.horas')).innerText,
+  }
+});
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const numMaior100 = numeros.filter((num) => {
+  return num > 100;
+});
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
+const possuiBaixo = instrumentos.some(item => item === 'Baixo');
+
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+const totalCompras = compras.reduce((acumulador, atual) => {
+  return acumulador += +(atual.preco.slice(2).replace(',', '.'));
+}, 0);
