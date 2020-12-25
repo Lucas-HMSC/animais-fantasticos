934dfe3752b09f5b6a4028ece14c0d9cf047151c
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 10:12:24 -0300

    Create ex011

--- exercises.js
+++ exercises.js
@@ -386,4 +386,34 @@ transacoes.forEach((item) => {
 });
 console.log(`Foi encontrado ${qtd} itens de Taxa.`);
 
+//=========================================================================
+/* Exercício 011  */
 
+// Retorne um número aleatório
+// entre 1050 e 2000
+let aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+console.log(aleatorio);
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+const arrayNumber = numeros.split(', ');
+console.log(Math.max(...arrayNumber));
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+const novaLista = new Array();
+let somaTotal = 0;
+function limpaPreco(lista) {
+  lista.forEach((preco) =>{
+    novaLista.push(Math.round(Number.parseFloat(preco.trim().slice(2).trim().replace(',', '.'))));
+  });
+  novaLista.forEach((novoPreco) => {
+    somaTotal += novoPreco;
+  });
+}
+limpaPreco(listaPrecos);
+console.log(novaLista); // [60, 100, 230, 200]
+console.log(somaTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})); // R$ 590,00
