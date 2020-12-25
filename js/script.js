f0f64f5f1322e828e5ab954a4280d635f16709b8
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 18:15:35 -0300

    Modal 2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ import initScrollSmooth from './modules/scroll-smooth.js';
 import initAnimationScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import iniTabNav from './modules/tabnav.js';
+import iniModal from './modules/modal.js';
+import initModal from './modules/modal.js';
 
 initScrollSmooth();
 initAnimationScroll();
 initAccordion();
 iniTabNav();
+initModal();
