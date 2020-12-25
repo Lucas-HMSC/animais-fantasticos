43d3683c7d1716c830de75d68c2606bf50406da9
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 12:58:15 -0300

    Create ex014

--- exercises.js
+++ exercises.js
@@ -533,3 +533,30 @@ const compras = [
 const totalCompras = compras.reduce((acumulador, atual) => {
   return acumulador += +(atual.preco.slice(2).replace(',', '.'));
 }, 0);
+
+//=========================================================================
+/* Exercício 014  */
+
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const textoParagrafo = document.querySelectorAll('p');
+const somaTotal = Array.prototype.reduce.call(textoParagrafo, (acumulador, item) => {
+  return acumulador + item.innerText.length;
+}, 0);
+console.log(somaTotal); // 655
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHTML = conteudo : null;
+  return elemento;
+}
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+const novaFuncao = criarElemento.bind(null, 'h1', 'titulo');
