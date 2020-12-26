b3fd185c801544a7312998f0726f38d4e464f058
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-26 10:24:33 -0300

    Dropdown Menu

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import initAccordion from './modules/accordion.js';
 import iniTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+
 
 
 initScrollSmooth();
@@ -12,3 +14,4 @@ initAccordion();
 iniTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
