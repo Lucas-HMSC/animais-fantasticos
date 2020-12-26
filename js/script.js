5eb0386a28e06207b51936b40605ff3f8940bcd9
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-25 21:31:55 -0300

    Tooltip

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ import initScrollSmooth from './modules/scroll-smooth.js';
 import initAnimationScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import iniTabNav from './modules/tabnav.js';
-import iniModal from './modules/modal.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
 
 initScrollSmooth();
 initAnimationScroll();
 initAccordion();
 iniTabNav();
 initModal();
+initTooltip();
