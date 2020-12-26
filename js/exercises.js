2585294fd9e9a54048fa6f6ffc7d2df123cbb01e
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-26 12:18:26 -0300

    Create ex016

--- js/exercises.js
+++ js/exercises.js
@@ -594,3 +594,47 @@ Object.freeze(configuracao);
 // protótipo de String e Array
 console.log(Object.getOwnPropertyNames(String.prototype));
 console.log(Object.getOwnPropertyNames(Array.prototype));
+
+/*
+ * Exercícios realizados durante o módulo de Efeitos no DOM
+*/
+
+//=========================================================================
+/* Exercício 016  */
+
+// Mude a cor da tela para azul e depois para vermelho a cada 2s.
+function mudarClase() {
+  document.body.classList.toggle('active');
+}
+
+setInterval(mudarClase, 2000);
+
+// Crie um cronometro utilizando o setInterval. Deve ser possível
+// iniciar, pausar e resetar (duplo clique no pausar).
+const iniciar  = document.querySelector('.iniciar');
+const pausar  = document.querySelector('.pausar');
+const tempo  = document.querySelector('.tempo');
+
+iniciar.addEventListener('click', iniciarTempo);
+pausar.addEventListener('click', pausarTempo);
+pausar.addEventListener('dblclick', resetarTempo);
+
+let i = 0;
+let timer;
+
+function iniciarTempo() {
+  timer = setInterval(() => {
+    tempo.innerText = i++;
+  }, 100);
+  iniciar.setAttribute('disabled', '');
+}
+
+function pausarTempo() {
+  clearInterval(timer);
+  iniciar.removeAttribute('disabled');
+}
+
+function resetarTempo() {
+  tempo.innerText = 0;
+  i = 0;
+}
