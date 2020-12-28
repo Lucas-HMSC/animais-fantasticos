b43f52ed9e0b89ed44311ea7d379b3bd75321780
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-27 21:16:15 -0300

    Create ex017

---
+++ js/script-fetch.js
@@ -0,0 +1,45 @@
+const inputCep = document.querySelector('#cep');
+const btnCep = document.querySelector('#btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.text())
+  .then(body => {
+    resultadoCep.innerText = body;
+  })
+}
+
+
+const btcDisplay = document.querySelector('.btc');
+
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+  .then(response => response.json())
+  .then(btcJson => {
+    btcDisplay.innerText = ('R$ ' + btcJson.BRL.buy).replace('.', ',');
+  })
+}
+//setInterval(fetchBtc, 1000 * 30);
+fetchBtc();
+
+
+const btnProxima = document.querySelector('.proxima');
+const paragrafoPiada = document.querySelector('.piada');
+function puxarPiada() {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(r => r.json())
+  .then(piada => {
+    paragrafoPiada.innerText = piada.value;
+  })
+}
+
+btnProxima.addEventListener('click', puxarPiada);
