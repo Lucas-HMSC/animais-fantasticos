1b044c6e8665c2611ea14d0998f9c5179e91eb43
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-28 14:42:05 -0300

    Create ex019

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -728,3 +728,58 @@ const active = callback => callback();
 active(function() {
   console.log('Função ativada');
 });
+
+//=========================================================================
+/* Exercício 019  */
+
+// Crie uma Factory Function semelhante ao jQuery
+
+function $$(selectedElements) {
+  const elements = document.querySelectorAll(selectedElements);
+
+  function hide() {
+    elements.forEach(element => {
+      element.style.display = 'none';
+    });
+    return this;
+  }
+
+  function show() {
+    elements.forEach(element => {
+      element.style.display = 'initial1';
+    });
+    return this;
+  }
+
+  function on(onEvent, callback) {
+    elements.forEach(element => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this;
+  }
+
+  function addClass(className) {
+    elements.forEach(element => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+
+  function removeClass(className) {
+    elements.forEach(element => {
+      element.classList.remove(className);
+    });
+    return this;
+  }
+
+  return {
+    elements,
+    hide,
+    show,
+    on, 
+    addClass,
+    removeClass,
+  }
+}
+
+const btns = $$('button');
