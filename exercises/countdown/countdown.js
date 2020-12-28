8df69d3eeb60271607349ef7bd48698355eaa695
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-28 12:06:59 -0300

    Update Countdown

--- exercises/countdown/countdown.js
+++ exercises/countdown/countdown.js
@@ -1,12 +1,12 @@
 export default class Countdown {
   constructor(futureDate) {
-    this._futureDate = futureDate;
+    this.futureDate = futureDate;
   }
   get _actualDate() {
     return new Date();
   }  
   get _futureDate() {
-    return new Date(this._futureDate);
+    return new Date(this.futureDate);
   }
   get _timeStampDiff() {
     return this._futureDate.getTime() - this._actualDate.getTime();
--- exercises/countdown/script.js
+++ exercises/countdown/script.js
@@ -1,7 +1,14 @@
 import Countdown from './countdown.js';
 
 const timeTo2021 = new Countdown('31 December 2020 23:59:59');
+const dias = document.querySelector('.dias');
+const horas = document.querySelector('.horas');
+const minutos = document.querySelector('.minutos');
+const segundos = document.querySelector('.segundos');
 
 setInterval(() => {
-  console.log(timeTo2021.hours);
+  dias.innerText = timeTo2021.total.days;
+  horas.innerText = timeTo2021.total.hours;
+  minutos.innerText = timeTo2021.total.minutes;
+  segundos.innerText = timeTo2021.total.seconds;
 }, 1000);
