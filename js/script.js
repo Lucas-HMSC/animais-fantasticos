333213e1598735a55b8cd976eab384c89a261c40
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-29 13:37:58 -0300

    fetch-animais.js refactored

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSmooth = new ScrollSmooth('[data-menu="smooth"] a[href^="#"]');
@@ -29,5 +29,6 @@ initAnimationScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
+
+fetchAnimais('./animaisapi.json', '.numeros-grid');
