d7ab42a07960e34b0c98af0cb665919304cc373a
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-29 09:59:56 -0300

    tabnav.js refactored

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSmooth from './modules/scroll-smooth.js';
 import initAnimationScroll from './modules/scroll-animacao.js';
 import Accordion from './modules/accordion.js';
-import iniTabNav from './modules/tabnav.js';
+import TabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -16,8 +16,10 @@ scrollSmooth.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
 initAnimationScroll();
-iniTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
